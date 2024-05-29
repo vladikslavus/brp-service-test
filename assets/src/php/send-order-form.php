ad16cd7420c40de59de2c9f44e4d4c4c2c70fa69
vladikslavus <vladax@list.ru>
2024-05-29 14:11:49 +0300

    send-order-form.php file is corrected

--- assets/src/php/send-order-form.php
+++ assets/src/php/send-order-form.php
@@ -5,22 +5,15 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 
   foreach($_POST as $key => $value) {
     $value=trim($value); // remove spaces at the beginning and at the end of the variable
-    // if (get_magic_quotes_gpc()) $value = stripslashes($value); // remove slashes if needed
     $_POST[$key]=$value; // all changes are written to the $_POST array
 
-    // then make changes that will only go into the file,
-    // and we don't need to output them in the form.
-    // $value=htmlspecialchars($value,ENT_QUOTES); // replace HTML characters with their equivalents
-    // $value=str_replace("\r","",$value); // replace all line translations
-    // $msg[$key]=$value; // and assign the new values to the elements of the array $msg
+    // Next nothing goes into the file, that's why we don't create $msg[$key] array
   }
 
   // then we make various checks
   // the most important thing is that the $err variable is not empty for any errors
   // that is the $err variable is an error flag and contains all error messages at the same time
   $err=0;
-  if (empty($_POST["name"])) $err=1;
-  if (strlen($_POST["name"]) < 2 || strlen($_POST["name"]) > 100) $err=1;
   if (strlen($_POST["phone"]) > 18  || strlen($_POST["phone"]) < 18) $err=1;
   $phone_ok = (preg_match("/^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/", $_POST["phone"]));
   if (!$phone_ok && $_POST["phone"]) $err=1;
@@ -33,14 +26,13 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
   if (!$err) {
 
     $mail="Сообщение с http://".$_SERVER['HTTP_HOST']."\n";
-    $mail.="Имя: ".$_POST['name']."\n";
     $mail.="Тел.: ".$_POST['phone']."\n";
     $mail.="Страница запроса: ".$_SERVER['HTTP_REFERER']."\n";
-    $mail.="Отвечать бессмысленно, электронной почты НЕТ, зато есть номер телефона!!!";
+    $mail.="Отвечать бессмысленно, с сайта http://".$_SERVER['HTTP_HOST']." приходит только номер телефона!!!";
 
-    $to="vladax@list.ru, vladax@yandex.ru";
+    $to="info@vextor.ru, vladax@list.ru, vladax@yandex.ru";
     $subject="=?UTF-8?B?" . base64_encode("Сообщение с ".$_SERVER['HTTP_HOST'])."?=";
-    $headers="Content-type: text/plain; charset=UTF-8 \r\n" .
+    $headers="Content-type: text/plain; charset=UTF-8 \r\n" . // !inmortant fir the correct charset displaying
              "From: <info@vextor.ru>";
 
     if(mail($to, $subject, $mail, $headers)) {
